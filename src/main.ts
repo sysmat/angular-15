e15373037f754fd868bfe2a669d1e04525de0f96
sysmat <tomaz.majerhold@gmail.com>
2023-05-08 13:25:28 +0200

    1 file modified, 1 added

--- src/main.ts
+++ src/main.ts
@@ -2,20 +2,44 @@ import 'zone.js/dist/zone';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { bootstrapApplication } from '@angular/platform-browser';
+import {
+  provideRouter,
+  RouterModule,
+  RouterOutlet,
+  Routes,
+} from '@angular/router';
+import { SubComponent } from './sub.component';
+
+const routes: Routes = [
+  {
+    path: 'sub/:id',
+    title: 'Prva stran',
+    component: SubComponent,
+  },
+];
 
 @Component({
   selector: 'my-app',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   template: `
     <h1>Hello from {{name}}!</h1>
-    <a target="_blank" href="https://angular.io/start">
-      Learn more about Angular 
-    </a>
+    <a
+        title="simplify router values"
+        [routerLink]="['/sub', 12]"
+        [queryParams]="{ order: 'popular'}"
+        >
+        <strong>Router values</strong>
+      </a>
+      <br>
+      <br>
+    <router-outlet></router-outlet>
   `,
 })
 export class App {
   name = 'Angular';
 }
 
-bootstrapApplication(App);
+bootstrapApplication(App, {
+  providers: [provideRouter(routes)],
+});
---
+++ src/sub.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sub',
+  standalone: true,
+  template: `
+    sub komponenta
+  `,
+})
+export class SubComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
