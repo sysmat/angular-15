6ae474c499810142993667b487c2b939d6c5d21d
sysmat <tomaz.majerhold@gmail.com>
2023-05-08 13:34:13 +0200

    1 file modified

--- src/sub.component.ts
+++ src/sub.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sub',
   standalone: true,
   template: `
-    sub komponenta
+    {{orgId}}
   `,
 })
 export class SubComponent implements OnInit {
-  constructor() {}
+  orgId: number = 0;
+  constructor(private readonly route: ActivatedRoute) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.orgId = +this.route?.snapshot?.paramMap?.get('id')!;
+  }
 }
